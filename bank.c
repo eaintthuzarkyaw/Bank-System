086a53f30fa791183ebb015daa3e52f7cbc2b521
nori <nori94713@gmail.com>
2023-03-05 17:53:50 +0630

    first commit

---
+++ bank.c
@@ -0,0 +1,18 @@
+//
+// Created by nori9 on 2/13/2023.
+//
+
+#include "stdio.h"
+#include "zoom_online_bank.h"
+
+int main(){
+
+    loadingAllDataFromFile();
+    printingAllData();
+    welcome();
+
+
+    return 0;
+}
+
+
